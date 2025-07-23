511959a3223bb823854e839cc9b29b4b099edd2a
bibekranjan12345 <9938798566.s@gmail.com>
2025-07-23 12:46:56 +0530

    Update ScreenShotUtil.java

--- src/main/java/com/STL/Utils/ScreenShotUtil.java
+++ src/main/java/com/STL/Utils/ScreenShotUtil.java
@@ -1,34 +1,29 @@
 package com.STL.Utils;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-public class ScreenShotUtil 
-{
+public class ScreenShotUtil {
     public static String captureScreenshot(WebDriver driver, String screenshotName) {
-        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-
-        // Save screenshot under test-output/ExtentReports/screenshots
-        String destFolder = System.getProperty("user.dir") + 
-                            "/test-output/ExtentReports/screenshots/";
-        File dir = new File(destFolder);
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-
+        String destFolder = System.getProperty("user.dir") + "/test-output/ExtentReports/screenshots/";
         String destPath = destFolder + screenshotName + ".png";
+
         try {
+            File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            File dir = new File(destFolder);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
             FileUtils.copyFile(src, new File(destPath));
-        } catch (IOException e) {
+        } catch (Exception e) {
+            System.out.println("⚠️ Screenshot capture failed: " + e.getMessage());
             e.printStackTrace();
         }
 
-        // Return path relative to HTML report
         return "./screenshots/" + screenshotName + ".png";
     }
 }
