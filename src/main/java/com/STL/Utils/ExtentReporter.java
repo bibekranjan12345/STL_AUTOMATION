f56660428ebca080540468f2baa24ee161318970
bibekranjan12345 <9938798566.s@gmail.com>
2025-08-02 11:57:37 +0530

    Modify only ExtentReport class and push it into New Regression Branch
    Modify only ExtentReport class and push it into New Regression Branch

--- src/main/java/com/STL/Utils/ExtentReporter.java
+++ src/main/java/com/STL/Utils/ExtentReporter.java
@@ -8,28 +8,39 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentReporter {
-    public static String generatedReportPath;  // ADD THIS
+    public static String generatedReportPath;
 
     public static ExtentReports generateExtentReport(String suiteName) {
         ExtentReports extentReport = new ExtentReports();
 
-        String sanitizedSuiteName = suiteName.replaceAll("\\s+", ""); // e.g., "SmokeSuite"
+        String sanitizedSuiteName = suiteName.replaceAll("\\s+", "");
         String timestamp = new java.text.SimpleDateFormat("yyyy-MM-dd_HH-mm").format(new java.util.Date());
         String reportDir = System.getProperty("user.dir") + "/test-output/ExtentReports";
         new java.io.File(reportDir).mkdirs();
 
-        String reportPath = reportDir + "/ExtentReport_" + sanitizedSuiteName + "_" + timestamp + ".html";
+        // 1. Timestamped file for historical tracking
+        String timestampedReportPath = reportDir + "/ExtentReport_" + sanitizedSuiteName + "_" + timestamp + ".html";
+        generatedReportPath = timestampedReportPath; // You can still use this for email/report sharing
 
-        generatedReportPath = reportPath; // SET THE PATH HERE
+        // 2. Static report for Jenkins HTML Publisher plugin
+        String staticReportPath = reportDir + "/ExtentReport_" + sanitizedSuiteName + ".html";
 
-        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(reportPath);
-        sparkReporter.config().setTheme(Theme.STANDARD);
-        sparkReporter.config().setReportName("STL Automation Execution Report : " + suiteName);
-        sparkReporter.config().setDocumentTitle("STL QA Execution Results");
-        sparkReporter.config().setTimeStampFormat("dd-MM-yyyy HH:mm:ss");
+        // Create both reporters
+        ExtentSparkReporter sparkTimestamped = new ExtentSparkReporter(timestampedReportPath);
+        ExtentSparkReporter sparkStatic = new ExtentSparkReporter(staticReportPath);
 
-        extentReport.attachReporter(sparkReporter);
+        // Common config for both
+        for (ExtentSparkReporter reporter : new ExtentSparkReporter[]{sparkTimestamped, sparkStatic}) {
+            reporter.config().setTheme(Theme.STANDARD);
+            reporter.config().setReportName("STL Automation Execution Report : " + suiteName);
+            reporter.config().setDocumentTitle("STL QA Execution Results");
+            reporter.config().setTimeStampFormat("dd-MM-yyyy HH:mm:ss");
+        }
 
+        // Attach both reporters
+        extentReport.attachReporter(sparkTimestamped, sparkStatic);
+
+        // Add system info
         try {
             Properties prop = new Properties();
             InputStream input = ExtentReporter.class.getClassLoader().getResourceAsStream("config.properties");
@@ -50,3 +61,91 @@ public class ExtentReporter {
         return extentReport;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package com.STL.Utils;
+//
+//import java.io.InputStream;
+//import java.util.Properties;
+//
+//import com.aventstack.extentreports.ExtentReports;
+//import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+//import com.aventstack.extentreports.reporter.configuration.Theme;
+//
+//public class ExtentReporter {
+//    public static String generatedReportPath;  // ADD THIS
+//
+//    public static ExtentReports generateExtentReport(String suiteName) {
+//        ExtentReports extentReport = new ExtentReports();
+//
+//        String sanitizedSuiteName = suiteName.replaceAll("\\s+", ""); // e.g., "SmokeSuite"
+//        String timestamp = new java.text.SimpleDateFormat("yyyy-MM-dd_HH-mm").format(new java.util.Date());
+//        String reportDir = System.getProperty("user.dir") + "/test-output/ExtentReports";
+//        new java.io.File(reportDir).mkdirs();
+//
+//        String reportPath = reportDir + "/ExtentReport_" + sanitizedSuiteName + "_" + timestamp + ".html";
+//
+//        generatedReportPath = reportPath; // SET THE PATH HERE
+//
+//        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(reportPath);
+//        sparkReporter.config().setTheme(Theme.STANDARD);
+//        sparkReporter.config().setReportName("STL Automation Execution Report : " + suiteName);
+//        sparkReporter.config().setDocumentTitle("STL QA Execution Results");
+//        sparkReporter.config().setTimeStampFormat("dd-MM-yyyy HH:mm:ss");
+//
+//        extentReport.attachReporter(sparkReporter);
+//
+//        try {
+//            Properties prop = new Properties();
+//            InputStream input = ExtentReporter.class.getClassLoader().getResourceAsStream("config.properties");
+//            if (input != null) {
+//                prop.load(input);
+//                extentReport.setSystemInfo("Application URL", prop.getProperty("url"));
+//                extentReport.setSystemInfo("Browser", prop.getProperty("browser"));
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        extentReport.setSystemInfo("Suite Name", suiteName);
+//        extentReport.setSystemInfo("OS", System.getProperty("os.name"));
+//        extentReport.setSystemInfo("User", System.getProperty("user.name"));
+//        extentReport.setSystemInfo("Java Version", System.getProperty("java.version"));
+//
+//        return extentReport;
+//    }
+//}
